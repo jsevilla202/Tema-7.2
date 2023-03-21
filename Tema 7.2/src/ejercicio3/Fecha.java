b28c3d51c64fbd8e6665918a3307ff033d977d2e
jsevilla202 <jsevilla@iesnervion.es>
2023-03-21 12:27:16 +0100

    Ejercicio 3 comentado con javadoc

--- Tema 7.2/src/ejercicio3/Fecha.java
+++ Tema 7.2/src/ejercicio3/Fecha.java
@@ -1,14 +1,36 @@
 package ejercicio3;
 
+/**
+ * Clase con las funciones de Fecha
+ * @author Jaime Sevilla
+ *
+ */
 public class Fecha {
+	/**
+	 * Parametro de dia
+	 */
 	private int dia;
+	/**
+	 * Parametro de mes
+	 */
 	private int mes;
+	/**
+	 * Parametro de año
+	 */
 	private int ano;
 	
+	/**
+	 * Constructor por defecto
+	 */
 	public Fecha() {
 		super();
 	}
-
+	/**
+	 * Constructor con todos los parametros
+	 * @param dia Recive el dia y se lo da al dia de la clase
+	 * @param mes Recive el mes y se lo da al mes de la clase
+	 * @param ano Recive el año y se lo da al año de la clase
+	 */
 	public Fecha(int dia, int mes, int ano) {
 		super();
 		this.dia = dia;
@@ -16,42 +38,72 @@ public class Fecha {
 		this.ano = ano;
 	}
 
+	/**
+	 * Getter de dia
+	 * @return Devuelve el valor de dia
+	 */
 	public int getDia() {
 		return dia;
 	}
-
+	
+	/**
+	 * Setter de dia. Comprueba que sea posible insertar el día especificado
+	 * @param dia Dia dado por el usuario
+	 */
 	public void setDia(int dia) {
 		if(mes == 2) {
 			if((ano%4==0 && dia>0&&dia<30) || dia>0&&dia<29) {
 				this.dia = dia;
 			}
 		}
-		else if(dia>0&&dia<32) {
+		else if(dia>=1&&dia<=31) {
 			this.dia = dia;
 		}
 	}
 
+	/**
+	 * Getter de mes
+	 * @return Devuelve el valor del mes
+	 */
 	public int getMes() {
 		return mes;
 	}
 
+	/**
+	 * Setter de mes. Comprueba que sea posible insertar el mes especificado
+	 * @param mes Mes dado por el usuario
+	 */
 	public void setMes(int mes) {
 		if(mes>0&&mes<13) {
 			this.mes = mes;
 		}
 		if(mes == 2 && dia>28) {
-			mes = 0;
+			this.mes = 1;
 		}
 	}
 
+	/**
+	 * Getter de año
+	 * @return Devuelve el valor de año
+	 */
 	public int getAno() {
 		return ano;
 	}
-
+	
+	/**
+	 * Setter de año. Comprueba si es posible establecer ese año
+	 * @param ano Año dado por el usuario
+	 */
 	public void setAno(int ano) {
-		this.ano = ano;
+		if(ano>=0) {
+			this.ano = ano;
+		}
 	}
 	
+	/**
+	 * Comprueba si el año es bisiesto
+	 * @return Devuelve true o false dependiendo si lo es o no
+	 */
 	boolean esBisiesto() {
 		boolean esBisiesto = false;
 		if(ano%4 == 0) {
@@ -60,6 +112,10 @@ public class Fecha {
 		return esBisiesto;
 	}
 	
+	/**
+	 * Comprueba si la fecha es correcta o no
+	 * @return Devuelve true o false si lo es o no
+	 */
 	boolean fechaCorrecta() {
 		boolean esCorrecta = false;
 		if(esBisiesto() == true) {
@@ -85,6 +141,9 @@ public class Fecha {
 		return esCorrecta;
 	}
 	
+	/**
+	 * Combierte la fecha en la del día siguiente
+	 */
 	void diaSiguiente() {
 		if(esBisiesto()==true) {
 			if(mes == 2) {
@@ -105,15 +164,42 @@ public class Fecha {
 				dia = 1;
 				mes++;
 			}
-			else if(dia<31 && mes == 12) {
-				dia ++;
-			}
-			else {
+			else if(dia == 31 && mes == 12){
 				dia = 1;
 				mes = 1;
 				ano++;
 			}
+			else {
+				dia++;
+			}
 		}
+	}
+	
+	/**
+	 * Combierte la fecha a String
+	 */
+	public String toString() {
+		String fecha="";
+		
+		if(dia/10 != 0) {
+			fecha += dia;
+		}
+		else {
+			fecha += "0" + dia;
+		}
+		
+		fecha += "-";
+		
+		if(mes/10 != 0) {
+			fecha += mes;
+		}
+		else {
+			fecha += "0" + mes;
+		}
+		
+		fecha += "-" + ano;
+		
+		return fecha;
 	}
 	
 	
--- Tema 7.2/src/ejercicio3/Main.java
+++ Tema 7.2/src/ejercicio3/Main.java
@@ -3,8 +3,40 @@ package ejercicio3;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Fecha fecha = new Fecha();
+		
+		System.out.println("dia " + fecha.getDia());
+		fecha.setDia(10);
+		System.out.println("dia " + fecha.getDia());
+		fecha.setDia(32);
+		System.out.println("dia " + fecha.getDia());
+		System.out.println("mes " + fecha.getMes());
+		fecha.setMes(10);
+		System.out.println("mes " + fecha.getMes());
+		fecha.setMes(13);
+		System.out.println("mes " + fecha.getMes());
+		System.out.println("año " + fecha.getAno());
+		fecha.setAno(2000);
+		System.out.println("año " + fecha.getAno());
+		System.out.println(fecha.fechaCorrecta()? "Es correcta" : "No es correcta");
+		fecha.setDia(31);
+		fecha.setMes(2);
+		System.out.println(fecha.fechaCorrecta()? "Es correcta" : "No es correcta");
+		System.out.println(fecha.getDia() + " " + fecha.getMes() + " " + fecha.getAno());
+		fecha.diaSiguiente();
+		System.out.println(fecha.getDia() + " " + fecha.getMes() + " " + fecha.getAno());
+		fecha.setMes(12);
+		fecha.setDia(31);
+		System.out.println("fin de año " + fecha.getDia() + " " + fecha.getMes() + " " + fecha.getAno());
+		fecha.diaSiguiente();
+		System.out.println(fecha.getDia() + " " + fecha.getMes() + " " + fecha.getAno());
+		System.out.println(fecha.toString());
+		fecha.setMes(12);
+		fecha.setDia(31);
+		System.out.println(fecha.toString());
+		System.out.println(fecha.esBisiesto()? "Es bisiesto" : "No es bisiesto");
+		fecha.setAno(2000);
+		System.out.println(fecha.esBisiesto()? "Es bisiesto" : "No es bisiesto");
 	}
 
 }
